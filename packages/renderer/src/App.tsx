17806b6e130d166d91921478424a8b6d6b256b73
myst <mystster@gmail.com>
2021-11-12 22:44:14 +0900

    fix: show full

--- packages/renderer/src/App.tsx
+++ packages/renderer/src/App.tsx
@@ -10,37 +10,7 @@ const App: React.FC = () => {
   
   return (
     <div className="App">
-      <header className="App-header">
-        <Viewer/>
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <Viewer full/>
     </div>
   );
 };
